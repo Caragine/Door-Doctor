dafff488042ff7aedcc9ccc4fc522a2bc0444bcc
Caragine <ac83779p@pace.edu>
2024-08-22 18:24:56 -0400

    approve review changes

--- door_doctor/reviews/views.py
+++ door_doctor/reviews/views.py
@@ -5,7 +5,7 @@ from .models import Review
 from .forms import ReviewForm
 from django.core.mail import send_mail
 from django.conf import settings
-from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
 import os
 import uuid
 # Create your views here.
